a30a7a313839534c4337e72c0bfabd8c55d5ed44
Ryan Ashby <rjapsu160@gmail.com>
2022-03-04 10:39:35 -0800

    update construtor for Title = title/ test PASS

--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -11,7 +11,7 @@ namespace VendorOrder.Models
 
     public Order (string title)
     {
-      Title = "test title";
+      Title = title;
     }
     
     public Order (string title, string description, string price, string date)
