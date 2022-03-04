6a56435757da504a800757176d7b7d4bee170616
Ryan Ashby <rjapsu160@gmail.com>
2022-03-04 12:33:04 -0800

    add test for Vendor contructor

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -8,6 +8,11 @@ namespace VendorOrder.Tests
   [TestClass]
   public class VendorTests
   {
-
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor vendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), vendor.GetType());
+    }
   }
 }
--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -4,6 +4,19 @@ namespace VendorOrder.Models
 {
   public class Vendor
   {
-    
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+
+     public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+     public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
   }
 }
